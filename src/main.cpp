0d3085e457ee86c96e3e3060abfd12ed2dcb559a
Reyansh Jajoo <reyanshja@outlook.com>
2025-08-03 16:43:47 -0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,84 @@
+#include "main.h"
+#include "lemlib/api.hpp"
+
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
+
+
+pros::MotorGroup leftMotors({-11, -12, -13}, pros::MotorGearset::blue);
+pros::MotorGroup rightMotors({18, 19, 20}, pros::MotorGearset::blue);
+
+pros::Imu imu(14);
+
+
+// drivetrain settings
+lemlib::Drivetrain drivetrain(&leftMotors,
+                              &rightMotors,
+                              11.2, // track width
+                              lemlib::Omniwheel::NEW_275,
+                              450,
+                              2 // horizontal drift
+);
+
+lemlib::ControllerSettings linearController(10, // kP
+                                            0, // kI
+                                            3, // kD
+                                            3, // anti windup
+                                            1, // small error range, in inches
+                                            100, // small error range timeout, in milliseconds
+                                            3, // large error range, in inches
+                                            500, // large error range timeout, in milliseconds
+                                            20 // maximum acceleration (slew)
+);
+
+lemlib::ControllerSettings angularController(2, // kP
+                                             0, // kI
+                                             10, // kD
+                                             3, // anti windup
+                                             1, // small error range, in degrees
+                                             100, // small error range timeout, in milliseconds
+                                             3, // large error range, in degrees
+                                             500, // large error range timeout, in milliseconds
+                                             0 // maximum acceleration (slew)
+);
+
+lemlib::OdomSensors sensors(nullptr, // vertical tracking wheel
+                            nullptr,
+                            nullptr, // horizontal tracking wheel
+                            nullptr, 
+                            &imu // inertial sensor
+);
+
+
+lemlib::Chassis chassis(drivetrain, linearController, angularController, sensors);
+
+void initialize() {
+    pros::lcd::initialize();
+    chassis.calibrate();
+
+    pros::Task screenTask([&]() {
+        while (true) {
+            pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
+            pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
+            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
+
+			lemlib::telemetrySink()->info("Chassis pose: {}", chassis.getPose());
+            pros::delay(50);
+        }
+    });
+}
+
+void disabled() {}
+
+void competition_initialize() {}
+
+void autonomous() {}
+
+void opcontrol() {
+    while (true) {
+        int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+        int rightY = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
+
+        chassis.tank(leftY, rightY);
+        pros::delay(10);
+    }
+}
