08f9e37f50cc0d53d025fd798834d6db09c36e35
Reyansh Jajoo <reyanshja@outlook.com>
2025-08-09 17:08:01 -0700

    intake logic and control

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,10 @@ pros::Controller controller(pros::E_CONTROLLER_MASTER);
 pros::MotorGroup leftMotors({-11, -12, -13}, pros::MotorGearset::blue);
 pros::MotorGroup rightMotors({18, 19, 20}, pros::MotorGearset::blue);
 
+pros::Motor firstStageIntake(-17, pros::MotorGearset::blue);
+pros::Motor basketRoller(2, pros::MotorGearset::green);
+pros::Motor hood(10, pros::MotorGearset::blue);
+
 pros::Imu imu(14);
 
 
@@ -51,10 +55,87 @@ lemlib::OdomSensors sensors(nullptr, // vertical tracking wheel
 
 lemlib::Chassis chassis(drivetrain, linearController, angularController, sensors);
 
+// intake bools
+bool runningIntake = false;
+bool runningBasket = false;
+bool outtake = false;
+bool midGoal = false;
+
+void toggle(){
+    while (true) {
+        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
+            runningIntake = !runningIntake;
+            if (runningBasket){
+                runningIntake = true;
+            }
+            runningBasket = false;
+            pros::delay(400);
+        }
+        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
+            if (midGoal != true){
+                runningBasket = !runningBasket;
+                runningIntake = runningBasket;
+            }
+            midGoal = false;
+            pros::delay(400);
+        }
+        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
+            if (midGoal != false){
+                runningBasket = !runningBasket;
+                runningIntake = runningBasket;
+            }
+            midGoal = true;
+            pros::delay(400);
+        }
+        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)){
+            outtake = true;
+        } else {
+            outtake = false;
+        }
+        pros::delay(20);
+    }
+}
+
+void intake_control() {
+    int hoodVel = 600;
+    while (true) {
+        if (runningIntake && !outtake && !runningBasket) {
+            firstStageIntake.move_velocity(600);
+            hood.move_velocity(600);
+            basketRoller.brake();
+        } else if (outtake && !runningBasket) {
+            firstStageIntake.move_velocity(-600);
+            hood.move_velocity(-600);
+            basketRoller.brake();
+        } else if (!runningIntake && !outtake && !runningBasket) {
+            firstStageIntake.move_velocity(0);
+            hood.move_velocity(0);
+            basketRoller.brake();
+        }
+
+        if (runningBasket && runningIntake && !outtake && !midGoal) {
+            firstStageIntake.move_velocity(600);
+            basketRoller.move_velocity(200);
+            hood.move_velocity(600);
+        } else if (runningBasket && runningIntake && !outtake && midGoal) {
+            basketRoller.move_velocity(200);
+            firstStageIntake.move_velocity(600);
+            hood.move_velocity(-600);
+        } else if (runningBasket && outtake) {
+            basketRoller.move_velocity(-200);
+            firstStageIntake.move_velocity(-600);
+            hood.move_velocity(-600);
+        }
+
+        
+        pros::delay(20);
+    }
+}
+
 void initialize() {
     pros::lcd::initialize();
     chassis.calibrate();
-
+    basketRoller.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
     pros::Task screenTask([&]() {
         while (true) {
             pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
@@ -74,6 +155,8 @@ void competition_initialize() {}
 void autonomous() {}
 
 void opcontrol() {
+    pros::Task intake_task(intake_control);
+    pros::Task toggle_task(toggle);
     while (true) {
         int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
         int rightY = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
